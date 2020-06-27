626031ba246cf7df2abba589deae32bd169a3f36
linchuhao <370356141@qq.com>
2020-06-27 15:21:57 +0800

    Java homework.

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        Student Lester = new Student();
+        Lester.setID(123);
+        Lester.setName("Lester");
+        Lester.setAge(22);
+        Lester.setGender("Male");
+        System.out.println(Lester.toString());
+    }
+}
---
+++ src/Student.java
@@ -0,0 +1,46 @@
+public class Student {
+
+    private int ID;
+
+    private String name;
+
+    private int age;
+
+    private String gender;
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String toString(){
+        return  "ID:"+ID + "Name:"+name + "Age:"+age + "Gender:"+gender;
+    }
+}
